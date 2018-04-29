126b6539196c914821e371d8e3f9e9b1ad83971f
Jared-See <pagaley12@outlook.com>
2018-04-29 17:49:02 -0400

    Added A function to check for A specefic Letter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,19 @@
 package com.company;
+
 //Libraries
 import java.util.Scanner;
-
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanInput = new Scanner(System.in); //Allows user input from cmd
-        //Ask the user for their input
-            System.out.print("Hello, this is a modular solution to the Project Euler problem #191, how many days would you like to solve for?");
+            //Ask the user for their input
+            System.out.print("Hello, this is a semi-modular solution to the Project Euler problem #191, how many days would you like to solve for?");
             int numDays = Integer.parseInt(scanInput.nextLine());
+
+            //Defines the Letters
+            char[] attendenceRec = new char[] {'A', 'L','O'};
+            StringExcersise.possibleStrings(30, attendenceRec, "");
         }
 
     }
@@ -33,5 +37,17 @@ public class Main {
                 }
             }
         }
+
+        public static int check_Letter (String base, char check){
+            int counter = 0;
+            for( int i=0; i<base.length(); i++ ) {
+                if( base.charAt(i) == check ) {
+                    counter++;
+                }
+            }
+            System.out.print(counter);
+            return counter;
+        }
     }
+
 
